ae6961e18e6b007a5fb3e77a0d96081625f07096
unknown <walexdavid59@gmail.com>
2018-04-05 18:42:12 +0100

    laravel template

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,19 @@
+<div class="navbar">
+    <div class="navbar-inner">
+       <h1> <a id="logo" href="/">Single Malt</a></h1>
+       <h3> 
+       <ul class="nav">
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/projects">Projects</a></li>
+            <li><a href="/contact">Contact</a></li>
+        </ul>
+    </h3>
+    </div>
+</div>
+
+<style>
+ul li{
+    padding: 10px;
+}
+</style>
---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,5 @@
+
+@extends('layouts.sidebar')
+@section('content')
+    i am the about page
+@stop
